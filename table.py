4d7b8c43ef786c39aefa490ef148e3a8656e508f
Jessica Kiroff <jesskiroff@gmail.com>
2025-11-17 20:03:44 -0500

    add pokemon and type to the ascii chart

--- table.py
+++ table.py
@@ -1,3 +1,5 @@
 from prettytable import PrettyTable
 table = PrettyTable()
+table.add_column("Pokemon",["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type",["Electric", "Water", "Fire"])
 print(table)
